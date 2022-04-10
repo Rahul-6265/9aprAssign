56d690b25fb0727ae2920461bf6a7e75888173ca
Rahul-6265 <rahulrc0907@gmail.com>
2022-04-10 17:17:02 +0530

    9aprassign

---
+++ apr9index.js
@@ -0,0 +1,11 @@
+function zoom() {
+    var imgElement = document.getElementById('Image');
+    imgElement.height = 700;
+    imgElement.width = 700;
+}
+
+function original() {
+    var imgElement = document.getElementById('Image');
+    imgElement.height = 300;
+    imgElement.width = 300;
+}
